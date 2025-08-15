4b2e1e0e9de3a25e4f426a4ac72424724c186b1a
Caio Aires <caioaires06@gmail.com>
2025-08-15 07:33:11 -0300

    Reviewed ALU code writing

--- source/design/alu.v
+++ source/design/alu.v
@@ -8,25 +8,21 @@ module alu #(
     output                  a_is_zero
 
 );
-    assign a_is_zero = (!in_a == 1)? 1'b1 
-                    :                1'b0;
+    assign a_is_zero = (!in_a == 1)? 1'b1 :
+                                     1'b0;
     
     always @*
     begin
         case(opcode)
-        3'b000 : alu_out = in_a;        //PASS A //HLT
-        3'b001 : alu_out = in_a;        //PASS A //SKZ
-        3'b010 : alu_out = in_a + in_b; //ADD
-        3'b011 : alu_out = in_a & in_b; //AND
-        3'b100 : alu_out = in_a ^ in_b; //XOR
-        3'b101 : alu_out = in_b;        //PASS B //LDA
-        3'b110 : alu_out = in_a;        //PASS A //STO
-        3'b111 : alu_out = in_a;        //PASS A //JMP
-        endcase
-    
+            3'b000 : alu_out = in_a;        //PASS A //HLT
+            3'b001 : alu_out = in_a;        //PASS A //SKZ
+            3'b010 : alu_out = in_a + in_b; //ADD
+            3'b011 : alu_out = in_a & in_b; //AND
+            3'b100 : alu_out = in_a ^ in_b; //XOR
+            3'b101 : alu_out = in_b;        //PASS B //LDA
+            3'b110 : alu_out = in_a;        //PASS A //STO
+            3'b111 : alu_out = in_a;        //PASS A //JMP
+        endcase    
     end
-
-
-
 
 endmodule
