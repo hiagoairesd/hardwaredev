27a0b28a0c361a6dbbd78be36f9ad7f9eb6c951e
Hiago Aires <hiago.emanuel.aires.diniz@ccc.ufcg.edu.br>
2025-08-22 16:22:27 -0300

    Update yosys synthesis scripts to be generic and include bash wrapper

--- synth/run_synth.sh
+++ synth/run_synth.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-# Check if passed top as argument
-if [ -z "$1" ]; then
-    echo "Uso: ./run_synth.sh <nome_do_top>"
+# Receives a top module as an argument
+TOP_MODULE=$1
+
+# Checks if argument was passed
+if [ -z "$TOP_MODULE" ]; then
     exit 1
 fi
 
-export TOP_MODULE=$1
-yosys -s synth.ys
+# Criates temporary script replacing TOP_MODULE
+TMP_SCRIPT=$(mktemp)
+sed "s/TOP_MODULE/$TOP_MODULE/g" synth_yosys.ys > $TMP_SCRIPT
+
+# Executes Yosys
+yosys -s $TMP_SCRIPT
+
+# Removes the temporary script
+rm $TMP_SCRIPT
