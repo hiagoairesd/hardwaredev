7640195afda3fedda09a19c3e3723052bd1c7448
Hiago Aires <hiago.emanuel.aires.diniz@ccc.ufcg.edu.br>
2025-08-28 15:04:51 -0300

    add converter onecold -> bin module

--- source/design/converter_onecold_bin.v
+++ source/design/converter_onecold_bin.v
@@ -1,4 +1,16 @@
 module converter_onecold_bin (
+    parameter WIDTH = 16
+) (
+    input  wire [WIDTH-1:0]         onecold_in,
+    output reg  [$clog2(WIDTH)-1:0] bin_out
+);
 
-)
+    integer i;
+    always @* begin
+        bin_out = {($clog2(WIDTH)){1'b0}};
+        for (i = WIDTH-1; i >= 0; i = i -1;) begin
+            if(!onecold_in[i])
+                bin_out = i;
+        end
+    end
 endmodule
