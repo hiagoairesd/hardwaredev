730151d884feebe6260126a1cb40a086ac16d322
Hiago Aires <hiagoairesd@gmail.com>
2025-12-05 08:51:52 -0300

    Update instruction memory source code

--- source/design/LASD/mod3/inst_mem.v
+++ source/design/LASD/mod3/inst_mem.v
@@ -6,7 +6,7 @@ module inst_mem #(
     input wire [DWIDTH-1:0] addr_in,
     output wire [DWIDTH-1:0] instruction_out,
 );
-
     reg [DWIDTH-1:0] mem [0:DEPTH-1];
-
+    
+    assign instruction_out = mem[addr_in];
 endmodule
