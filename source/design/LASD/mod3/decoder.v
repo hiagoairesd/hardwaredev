8bdbf2104679b5cb7703322a1ba4049083e84573
Hiago Aires <hiagoairesd@gmail.com>
2025-12-11 15:51:12 -0300

    Add instruction decoder source code

--- source/design/LASD/mod3/decoder.v
+++ source/design/LASD/mod3/decoder.v
@@ -1,8 +1,32 @@
 module decoder #(
-    parameter WIDTH = 32
+    parameter CTRL_WIDTH = 10,
+    parameter INSTR_WIDTH = 32
 ) (
-    input  wire [WIDTH-1:0] inst,
-    output wire [9:0]       word
+    input  wire [INSTR_WIDTH-1:0] instr,
+    output wire [CTRL_WIDTH-1:0]  word
 );
-    assign word = inst[9:0];
+
+    wire [0:5] opcode = [31:25] instr;
+    wire [0:5] funct = [5:0] instr;
+
+
+    always @*
+        case (opcode) 
+            6'b000000: begin    // R-TYPE INSTRUCTION
+                case (funct)
+                    6'b100000: word = 10'b1100100000;   // ADD
+                    6'b100010: word = 10'b1101100000;   // SUB
+                    6'b100100: word = 10'b1100000000;   // AND
+                    6'b100101: word = 10'b1100010000;   // OR
+                    6'b101010: word = 10'b1101110000;   // SLT
+                endcase
+            end
+            
+            6'b100011: word = 10'b1010100010;   // LW   (load word)
+            6'b101011: word = 10'b0x101001x0;   // SW   (store word)
+            6'b000100: word = 10'b0x011010x0;   // BEQ  (branch if equal)
+            6'b001000: word = 10'b1010100000;   // ADDi (add imm)
+            6'b000010: word = 10'b0xxxxxxxx1;   // JMP  (jump)
+            default:   word = 10'bxxxxxxxxxx;
+        endcase
 endmodule
