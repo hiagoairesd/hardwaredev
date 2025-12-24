3ffbc0c72b22c1d31fd4de272a4768a8790256b9
Hiago Aires <hiago.emanuel.aires.diniz@ccc.ufcg.edu.br>
2025-12-24 16:33:40 -0300

    Add data memory module

---
+++ source/design/LASD/mod3/data_mem.v
@@ -0,0 +1,22 @@
+module data_mem #(
+    parameter AWIDTH = 5,
+    parameter DWIDTH = 8
+) (
+    input   wire                clk,
+    input   wire                wr,
+    input   wire                rd,
+    input   wire [AWIDTH-1:0]   addr,
+    inout   wire [DWIDTH-1:0]   data
+);
+    
+    reg [DWIDTH-1 :0] mem [0:2**AWIDTH-1];
+
+    always @(posedge clk) begin
+        if(wr)
+            mem[addr] <= data;
+    end
+
+
+    assign data= (rd)? mem[addr] : {DWIDTH{1'bz}};
+
+endmodule
