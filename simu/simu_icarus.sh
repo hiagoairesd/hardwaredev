47f9b9830558fb70e813018140d94469522c8760
Hiago Aires <hiagoairesd@gmail.com>
2026-03-02 03:48:03 -0300

    Fixing bugs: fix ./simu_icarus.sh script

--- simu/simu_icarus.sh
+++ simu/simu_icarus.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
-ROOT="/home/hiago/Dev/hardwaredev/source"
+set -eo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+ROOT="$(realpath "$SCRIPT_DIR/../source")"
+
 DESIGN="$ROOT/design"
 VERIF="$ROOT/verif"
-SINGLE_CYCLE="$DESIGN/single_cycle"
 
 if [[ $# -lt 1 ]]; then
   echo "Usage: $0 <dut> [vvp_args...]" >&2
@@ -16,23 +19,11 @@ shift
 out="${dut}.out"
 tb="${VERIF}/${dut}_test.sv"
 
-deps=(
-  "$SINGLE_CYCLE/registers_bank.v"
-  "$SINGLE_CYCLE/alu.v"
-  "$SINGLE_CYCLE/data_mem.v"
-  "$SINGLE_CYCLE/instr_mem.v"
-  "$SINGLE_CYCLE/control_unit.v"
-)
-
 echo "###############################################################"
 echo "DUT : $dut"
 echo "TB  : $tb"
 
-# Seache RTL file in design/ 
-rtl="$(
-  find "$DESIGN" -type f \( -name "${dut}.v" -o -name "${dut}.sv" \) 2>/dev/null \
-  | head -n 1 || true
-)"
+rtl="$(find "$DESIGN" -type f \( -name "${dut}.v" -o -name "${dut}.sv" \) -print -quit)"
 
 if [[ -z "$rtl" ]]; then
   echo "ERROR: RTL not found for DUT '$dut' under: $DESIGN" >&2
@@ -46,21 +37,14 @@ fi
 
 echo "RTL : $rtl"
 echo "###############################################################"
-echo "### Running Compilation and Elaboration...#####################"
+echo "### Running Compilation and Elaboration... ####################"
 echo "###############################################################"
 
-if [[ "$dut" == "cpu_top" ]]; then
-  echo "MODE: cpu_top (with deps)"
-  iverilog -g2012 -o "$out" \
-    "$rtl" \
-    "${deps[@]}" \
-    "$tb"
-else
-  echo "MODE: single-module (no deps)"
-  iverilog -g2012 -o "$out" \
+iverilog -g2012 -o "$out" \
+    -y "$DESIGN" \
+    -y "$DESIGN/single_cycle" \
     "$rtl" \
     "$tb"
-fi
 
 echo "###############################################################"
 echo "### Running Simulation... #####################################"
